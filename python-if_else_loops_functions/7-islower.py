2e8c9da311f571712a0409df0a407939dc31fcec
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-10-02 20:39:51 +0400

    Lowercase confirm

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,19 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-    if type(c) != str or len(c) != 1:
-        raise TypeError("Error: input must be a single character")
-        
     if ord('a') <= ord(c) <= ord('z'):
         return True
     else:
         return False
-
-print("a is {}".format("lower" if islower("a") else "upper"))
-print("H is {}".format("lower" if islower("H") else "upper"))
-print("A is {}".format("lower" if islower("A") else "upper"))
-print("3 is {}".format("lower" if islower("3") else "upper"))
-print("g is {}".format("lower" if islower("g") else "upper"))
-
-print(islower("Hello"))
-print(islower(123))
-print(islower(""))
