41e7755a3744b88ef1ffe0de9589a2bcbfade306
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-10-02 12:03:27 +0400

    Prints 2 digits

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    
-    if i < 10:
-        i = print("0{}".format(i))
-    print("{}, ".format(i), end="")
+    if i < 99:
+        print("{:02d}, ".format(i), end="")
+    else:
+        print("{:02d}".format(i))
     
