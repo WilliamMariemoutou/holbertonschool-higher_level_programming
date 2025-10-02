6d9c75640c560935e7b86fd6770edf369816f6b4
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-10-02 10:04:39 +0400

    Don't print q and e

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -2,6 +2,6 @@
 
 alphabet = ""
 for i in range(97, 123):
-    if i != 101 and != 113:
-    alphabet += "{}".format(chr(i))
-print(alphabet)
+    if i != 101 and i != 113:
+        alphabet += "{}".format(chr(i))
+print(alphabet, end="")
