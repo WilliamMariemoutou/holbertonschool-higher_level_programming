4120be2bf9a8927f573cbae224a4fa62d5cc2db2
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-10-03 20:38:33 +0400

    Arguments

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+
+
+def main():
+    arguments = sys.argv[1:]
+    num_arguments = len(arguments)
+    
+    if num_arguments == 0:
+        print("0 arguments.")
+    else:
+        print("{} argument{}".format(num_arguments, "s" if num_arguments > 1 else ""))
+        for i, arg in enumerate(arguments, 1):
+            print("{}: {}".format(i, arg))
+            
+            
+if __name__ == "__main__":
+    main()
