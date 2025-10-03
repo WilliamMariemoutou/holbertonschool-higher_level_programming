86fa7491cc1bd7815be3ab911a7d839b0c1b3280
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-10-03 20:49:54 +0400

    Arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,14 +5,15 @@ import sys
 def main():
     arguments = sys.argv[1:]
     num_arguments = len(arguments)
-    
+
     if num_arguments == 0:
         print("0 arguments.")
     else:
-        print("{} argument{}:".format(num_arguments, "s" if num_arguments > 1 else ""))
+        print("{} argument{}:".format(num_arguments,
+            "s" if num_arguments > 1 else ""))
         for i, arg in enumerate(arguments, 1):
             print("{}: {}".format(i, arg))
-            
+
             
 if __name__ == "__main__":
     main()
