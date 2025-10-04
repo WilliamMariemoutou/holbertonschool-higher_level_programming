2cf23da771438c5960cad5e71c059faee3712764
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-10-04 23:21:58 +0400

    Hidden

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,24 +1,11 @@
-#!/usr/bin/env python3
-import marshal
-import os
-
-def discover_names():
-    pyc_file_path = '/tmp/hidden_4.pyc'
-
-    if not os.path.exists(pyc_file_path):
-        print(f"Error: {pyc_file_path} does not exist.")
-        return
-
-    with open(pyc_file_path, 'rb') as file:
-        file.seek(16)
-        code_object = marshal.load(file)
-
-    names = dir(code_object)
-    filtered_names = [name for name in names if not name.startswith('__')]
-    filtered_names.sort()
-
-    for name in filtered_names:
-        print(name)
+import sys
 
 if __name__ == "__main__":
-    discover_names()
+    sys.path.append('/tmp')
+    import hidden_4
+
+    names = dir(hidden_4)
+
+    for name in sorted(names):
+        if not name.startswith('__'):
+            print(name)
