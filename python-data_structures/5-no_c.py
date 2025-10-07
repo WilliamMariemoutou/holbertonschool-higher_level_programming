9ccdf225a5c60e5d878d43ddfdc93e683eec4e0a
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-10-07 18:14:44 +0400

    Remove c

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    letter = "c"
-    new_list = []
-    
-    for i in my_string:
-            if s != letter:
-                new_s += i
-    print(new_list)
+    new_string = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
+    return new_string
