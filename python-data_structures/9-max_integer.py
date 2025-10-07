fecaca9f1974bc07faf6bd6f3f86eb52563a1e33
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-10-07 19:36:21 +0400

    Prints max integer

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -5,7 +5,8 @@ def max_integer(my_list=[]):
 
     max_value = my_list[0]
 
-    for num in my_llist:
-        if num > max_value = num
+    for num in my_list:
+        if num > max_value:
+            max_value = num
 
     return max_value
