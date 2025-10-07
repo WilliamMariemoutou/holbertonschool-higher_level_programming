c76b74bb2d67c31d40335af167756656ab1ebd96
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-10-07 19:14:54 +0400

    Add tuples

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -6,5 +6,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_b) < 2:
         tuple_b = tuple_b + (0,) * (2 - len(tuple_b))
         
+    tuple_a = tuple_a[:2]
+    tuple_b = tuple_b[:2]
+        
     result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     print("{}, {})".format(result[0], result[1]))
