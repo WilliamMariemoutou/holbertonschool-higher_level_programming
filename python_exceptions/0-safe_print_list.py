bd3814d07210d184276efc45144ef07fd416a9b0
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-10-10 21:14:00 +0400

    Elements of a list

--- python_exceptions/0-safe_print_list.py
+++ python_exceptions/0-safe_print_list.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
@@ -8,6 +7,4 @@ def safe_print_list(my_list=[], x=0):
     except IndexError:
         pass
     print()
-    print(f"Debug: Printed {count} elements.")
     return count
- 
