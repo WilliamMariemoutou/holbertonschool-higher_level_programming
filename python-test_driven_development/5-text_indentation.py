e1b03c16c1e1bc572dd96cd54a4270f6b76ed174
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-10-11 18:40:28 +0400

    Indents a text

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    
+    i = 0
+    while i < len(text):
+        if text[i] in ['.', '?', ':']:
+            print(text[i], end="\n\n")
+            i += 1
+            while i < len(text) and text[i] == ' ':
+                i += 1
+        else:
+            print(text[i], end="")
+            i += 1
