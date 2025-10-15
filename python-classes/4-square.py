09e81c22768651ab5f6ed7fbcf3d6353c02b94f5
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-10-15 13:55:45 +0400

    Defines a square

---
+++ python-classes/4-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        self.size = size
+        
+    def size(self):
+        return self.__size
+    
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
