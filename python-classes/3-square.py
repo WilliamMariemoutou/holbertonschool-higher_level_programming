57806f760e22fe8331b4b0d57fd51e865bbff293
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-10-15 13:24:16 +0400

    Area of a square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -3,12 +3,12 @@ class Square:
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError
-        
+
         if size < 0:
             raise ValueError("size must be >= 0")
-        
+
         self.__size = size
-        
-        def area(self):
-            
-            return self.__size ** 2
+
+    def area(self):
+
+        return self.__size ** 2
