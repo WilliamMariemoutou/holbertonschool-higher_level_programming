3e46dde0fd3f62007c61fdd1f09f7b1cc07e0aa7
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-10-15 19:41:46 +0400

    Square error

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
+"""This class defines a square"""
+
+
 class Square:
+    """This class defines a square with a specific size"""
     def __init__(self, size=0):
+        """
+        Initialize a new square instance.
+        size: the size of the square
+        """
         if not isinstance(size, int):
             raise TypeError
 
         if size < 0:
             raise ValueError("size must be >= 0")
+        """
+        TypeError: if size is not an integer
+        ValueError: if size is less than 0
+        """
 
         self.__size = size
+        """the size of the square, must not be negative"""
