8116c6f7de3deeb570193ac61577e3dcfb4ba426
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-10-15 14:02:27 +0400

    Defines a square

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -2,14 +2,15 @@
 class Square:
     def __init__(self, size=0):
         self.size = size
-        
+
+    @property
     def size(self):
         return self.__size
-    
+
+    @size.setter
     def size(self, value):
         if not isinstance(value, int):
-            raise TypeError
-
+            raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
