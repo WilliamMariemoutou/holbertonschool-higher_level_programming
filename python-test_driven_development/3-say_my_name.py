4a8bbfa38ae1f7b009aa07a4762dc06a81e9e4c7
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-10-18 17:55:13 +0400

    Writes name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -19,8 +19,7 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    # Check if last_name is empty, and adjust formatting accordingly
     if last_name:
         print("My name is {} {}".format(first_name, last_name))
     else:
-        print("My name is {} ".format(first_name))
+        print("My name is {}".format(first_name))
