a86072475c761379b85a148b52c99fd92a117c24
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-10-18 18:54:39 +0400

    Put indents

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -16,10 +16,14 @@ def text_indentation(text):
     i = 0
     while i < len(text):
         if text[i] in ['.', '?', ':']:
+            """checks if we see the required symbols"""
             print(text[i], end="\n\n")
             i += 1
+            """prints the character with a newline"""
             while i < len(text) and text[i] == ' ':
                 i += 1
+                """ignore spaces afther the symbol"""
         else:
             print(text[i], end="")
             i += 1
+            """prints current character"""
