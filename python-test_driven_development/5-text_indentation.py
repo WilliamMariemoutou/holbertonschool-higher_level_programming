01f2622460cbbee1eebdf93a4fd92c4b4adc840c
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-10-18 18:45:21 +0400

    Put indents

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -3,7 +3,7 @@
 def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    
+
     i = 0
     while i < len(text):
         if text[i] in ['.', '?', ':']:
