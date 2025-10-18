afe2d41b6b72b24315497fd846a618249bd7bd03
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-10-18 19:12:00 +0400

    Put indents

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-"""Prints a text with two new lines after each characters."""
-
+"""Prints a text with one new line after each of the characters `.`, `?`, and `:`."""
 
 def text_indentation(text):
     """
+    Prints a text with one new line after each of these characters: `.`, `?`, and `:`.
 
     Parameters:
         text (str): The string to print, which must be of type string.
@@ -15,17 +15,18 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    i = 0
-    while i < len(text):
-        if text[i] in ['.', '?', ':']:
-            """checks if we see the required symbols"""
-            print(text[i], end="\n\n")
-            i += 1
-            """prints the character with a newline"""
-            while i < len(text) and text[i] == ' ':
+    if any(c in text for c in ['.', '?', ':']):
+        i = 0
+        while i < len(text):
+            if text[i] in ['.', '?', ':']:
+                print(text[i], end="\n")
                 i += 1
-                """ignore spaces afther the symbol"""
-        else:
-            print(text[i], end="")
-            i += 1
-            """prints current character"""
+
+                while i < len(text) and text[i] == ' ':
+                    i += 1
+            else:
+                print(text[i], end="")
+                i += 1
+    else:
+        print(text)
+
