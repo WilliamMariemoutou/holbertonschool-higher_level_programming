06eef549a5cf10a275faa7c2318902c74ca28d7a
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-10-18 18:47:46 +0400

    Put indents

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 
 def text_indentation(text):
+    """
+    Prints a text with two new lines after each of these characters: `.`, `?`, and `:`.
+
+    Parameters:
+        text (str): The string to print, which must be of type string.
+
+    Raises:
+        TypeError: If `text` is not a string.
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
