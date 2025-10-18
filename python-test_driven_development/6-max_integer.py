d5c5e9c101af9624cf07a7da12e980ca1f1bcc56
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-10-18 19:22:39 +0400

    Finds max integer

--- python-test_driven_development/6-max_integer.py
+++ python-test_driven_development/6-max_integer.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python3
-"""function to find max integer"""
-
+"""Module to find the max integer in a list
+"""
 
 def max_integer(list=[]):
     """Function to find and return the max integer in a list of integers.
-    If the list is empty, the function returns None.
+       If the list is empty, the function returns None.
+
+    Args:
+        list (list): A list of integers.
+
+    Returns:
+        int: The maximum integer in the list, or None if the list is empty.
+
+    Raises:
+        TypeError: If any element in the list is not an integer.
     """
     if len(list) == 0:
         return None
+    
+    # Check if all elements in the list are integers
+    for elem in list:
+        if not isinstance(elem, int):
+            raise TypeError("All elements in the list must be integers")
+    
     result = list[0]
     i = 1
     while i < len(list):
