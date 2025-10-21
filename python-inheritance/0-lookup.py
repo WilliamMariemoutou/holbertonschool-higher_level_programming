e4baa90db6a9ba6e88a97600bd01a4c82926d895
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-10-21 13:24:29 +0400

    List of attributes and methods

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""creates a class of object"""
+
+
 def lookup(obj):
+    """the class of object"""
     return dir(obj)
+
+
+"""returns the list of methods and attributes of the class"""
