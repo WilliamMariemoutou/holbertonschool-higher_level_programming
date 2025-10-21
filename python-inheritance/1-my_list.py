31982098d25c9ea4c3101ae7138744f0c4640539
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-10-21 22:33:44 +0400

    Prints a list in order

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,8 +4,8 @@
 
 class MyList(list):
     """the subclass of the list, prints in ascending order"""
-    
+
     def print_sorted(self):
         """prints the elements of the list in ascending order"""
-        
+
         print(sorted(self))
