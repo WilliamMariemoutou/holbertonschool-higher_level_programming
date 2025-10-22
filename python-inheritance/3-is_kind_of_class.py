dc59c6960be15018e146f3bdc8cb07727adcd937
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-10-22 22:30:31 +0400

    Instance of an inherited class

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""the class that needs to be checked"""
+
+
+def is_kind_of_class(obj, a_class):
+    """check if object is an instance of a specified class
+    obj: object to check
+    returns: true if obj is an instance of the current class
+    """
+    return isinstance(obj, a_class)
