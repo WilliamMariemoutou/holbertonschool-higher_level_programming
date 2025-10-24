bb8c348b34ece509307450a4fcea63d426e85412
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-10-24 13:29:07 +0400

    Shapes, duck typing

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -42,5 +42,5 @@ class Rectangle(Shape):
 
 def shape_info(shape):
     """shape_info duck typing"""
-    print(f"Area:  {shape.area()}")
-    print(f"Perimeter:  {shape.perimeter()}")
+    print("Area: ",shape.area())
+    print("Perimeter: ",shape.perimeter())
