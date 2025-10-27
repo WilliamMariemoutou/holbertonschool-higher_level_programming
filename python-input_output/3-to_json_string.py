3e202ed9ca267ed62c7ab613e411c367bdf4d2ed
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-10-27 21:28:34 +0400

    JSON string representation

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+"""JSON string representation"""
+
+
+def to_json_string(my_obj):
+    """returns the JSON representation of an object"""
+    return json.dumps(my_obj)
