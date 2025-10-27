b2a19c302ece6b18a0c0fd3b388719e63b49df8f
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-10-27 21:19:35 +0400

    Appends a string and return the number of characters added

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""appends a string at the end of a text file"""
+
+
+def append_write(filename="", text=""):
+    """apeends a string and return the number of characters added"""
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
