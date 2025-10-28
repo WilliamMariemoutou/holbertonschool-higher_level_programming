077833fc335117fbf6e5a3ac3138e360f2569af4
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-10-28 20:39:06 +0400

    Student

---
+++ python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""creates a class student"""
+
+
+class Student:
+    """defines a student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """initialize a new student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """return the dictionary representation of the student class
+        if attrs is a list of string, retrieves the attributes name
+        else returns all attributes
+        """
+        if isinstance(attrs, list) and all(type(a) == str for a in attrs):
+            result = {}
+            for key in attrs:
+                if hasattr(self, key):
+                    result[key] = getattr(self, key)
+            return result
+        
+        return self.__dict__
