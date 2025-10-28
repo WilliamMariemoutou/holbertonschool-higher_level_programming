dbb4aa23630cc940ae3bb1feb072c733ae0572ed
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-10-28 20:10:42 +0400

    Replaces all attributes

---
+++ python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""creates a class student"""
+
+
+class Student:
+    """defines a student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """initialize a new student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """return the dictionary representation of the student class
+        if attrs is a list of string, retrieves the attributes name
+        else returns all attributes
+        """
+        if isinstance(attrs, list):
+            result = {}
+            for key in attrs:
+                if isinstance(key, str) and hasattr(self, key):
+                    result[key] = getattr(self, key)
+                return result
+
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """replaces all attributes f the student instance"""
+        for key, value in json.items():
+            setattr(self, key, value)
