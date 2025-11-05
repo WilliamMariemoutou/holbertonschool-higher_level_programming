90efa0012cac6bf051f1ed56b9473bfa25409047
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-11-05 22:46:48 +0400

    HTTPS server

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -40,6 +40,13 @@ class MyRequestHandler(http.server.BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(json.dumps(status).encode('utf-8'))
 
+    def handle_info(self):
+        info = {"version": "1.0", "description": "A simple API built with http.server"}
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps(info).encode('utf-8'))
+
     def handle_404(self):
 
         self.send_response(404)
