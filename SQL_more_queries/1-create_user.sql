6da960f35ac0f28d28967c71a2bdfe162e27d522
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-11-20 21:01:32 +0400

    Creates a new user with all privileges

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,7 @@
+-- Creates a user with all privileges and password
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+FLUSH PRIVILEGES;
