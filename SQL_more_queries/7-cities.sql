f311febc32af99e1f10835dcfd8ab7382ea88b16
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-11-21 22:11:16 +0400

    Creates table cities

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- Creates a new datasbase in hbtn_0d_usa
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS cities (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
