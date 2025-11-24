6d972f259d76e5c49dfceeea94bece21899d7c58
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-11-24 22:26:53 +0400

    Filter all states by the letter N

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 
-"""lists all states with a name starting with N form the database hbtn_0e_0_usa.
+"""
+lists all states with a name starting with N form the database hbtn_0e_0_usa.
 """
 
 import MySQLdb
