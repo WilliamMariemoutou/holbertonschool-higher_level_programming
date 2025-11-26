937abff915f90e3c0e2bfb92eea2682e66e3ca69
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-11-26 22:09:58 +0400

    Model city

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
-"""
-Class definition for City that maps to the cities table in MySQL
-"""
-
+"""City class definition"""
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from model_state import Base
 
 
 class City(Base):
-    """
-    Class representing a City linked to the 'cities' table in the database
-    """
+    """City class mapped to cities table"""
     __tablename__ = 'cities'
 
-    # Define columns
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-
-    # Relationship with the State table (optional, but can be useful for ORM)
-    state = relationship("State", back_populates="cities")
+    state = relationship("State")
