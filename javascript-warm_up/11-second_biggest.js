824dae19c1c764bda2be3ebc096c98ba9e8f9524
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-12-06 01:15:27 +0400

    Second biggest!

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,7 @@ const args = process.argv.slice(2).map(x => parseInt(x, 10));
 if (args.length < 2) {
   console.log(0);
 } else {
-  let max = Math.max(...args);
-  let filtered = args.filter(n => n !== max);
+  const max = Math.max(...args);
+  const filtered = args.filter(n => n !== max);
   console.log(filtered.length === 0 ? 0 : Math.max(...filtered));
 }
