4423e1f42a5f670b4b00b1cd15b2f31260e446a4
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-12-09 13:32:53 +0400

    Change the text

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,7 @@
+const header = document.querySelector('#update_header');
+
+header.onclick = function () {
+    const header = document.querySelector('header');
+
+    header.textContent = 'New Header!!!';
+};
